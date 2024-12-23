f91b6db2d8219be6ffa225cb525472444469fe2e
Ahmad Hamesh <ahmadhamesh4@gmail.com>
2024-12-24 02:56:18 +0530

    custom react app

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,11 @@
-
+import Chai from "./Chai";
 
 function App() {
   return (
+    <>
     <h1>chai aur react | Ahmad Hamesh |Created React Project and with using vite </h1>
+    <Chai/>
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,16 @@
-
+import Chai from "./chai"
 
 function App() {
-  
+  const username = 'chai aur code'
 
   return (
-    <h1>chai aur react | with vite</h1>
+    <>
+      <h1>chai aur react | with vite| {username}</h1>
+      {/* {} this curly brecket is expression or evaluated expresion */}
+      <Chai />
+    </>
+    
+
   )
 }
 
